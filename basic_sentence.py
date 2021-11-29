5ac135d052f2d2df3ad95f75470df3486a1ceb2a
goharm <mrgoharm@gmail.com>
2021-11-29 16:38:13 +0900

    문법 정리

--- basic_sentence.py
+++ basic_sentence.py
@@ -3,12 +3,12 @@ def func_01():
     pass
     return
 
-try:
-    pass
-
-except Exception as e: 
-    pass
 
 
 if __name__ = '__main__':
-    pass
+    try:
+        pass
+
+    except Exception as e: 
+        pass
+    
