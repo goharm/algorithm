ef2c80e2b4abc0b0a25f9043562f4c256cd19f5a
macbook_고함 <ceo@snmp.kr>
2021-12-01 17:33:21 +0900

    콜백함수 이해 안됨?

--- callback_02.py
+++ callback_02.py
@@ -1,7 +1,15 @@
-def func_callback():
+def func_callback(input, extend_input):
+    print('func_callback sum : ', input)
+    print('extend', extend_input)
     return
 
-def func_call():
+def func_call(one, two, f_callback):
+    result = one + two
+    f_callback(result)
     return
 
-func_call()
+
+first = 10
+second = 20
+third = 30
+func_call(first, second, func_call, third)
